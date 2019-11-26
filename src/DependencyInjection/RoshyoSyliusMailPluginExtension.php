a34d860a91f9450753bdcd73d537ff045e403b1f
Roshyo <stefdecock68@gmail.com>
2019-11-26 22:23:44 +0100

    Switching to yaml for services

--- src/DependencyInjection/RoshyoSyliusMailPluginExtension.php
+++ src/DependencyInjection/RoshyoSyliusMailPluginExtension.php
@@ -7,18 +7,15 @@ namespace Roshyo\SyliusMailPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class RoshyoSyliusMailPluginExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $loader->load('services.xml');
+        $loader->load('services.yaml');
     }
 }
